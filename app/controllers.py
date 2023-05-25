9a590fdb8618174d65dfcd341829dd2f5a26ccfb
OmarSabbah999 <omars@mail.tau.ac.il>
2023-05-25 16:31:53 +0300

    logic changes

--- app/controllers.py
+++ app/controllers.py
@@ -1,37 +1,44 @@
 from flask import jsonify
 from .models import Session, Order
 
+
+
 def get_all_orders():
     session = Session()
     orders = session.query(Order).all()
-
+    # print(orders)
     data = []
     for order in orders:
         data.append({
+            "order_id":order.id,
             "user_id":order.user_id,
             "stock id":order.stock_id,
             "status":order.executed,
             "created at":order.created_at
         })
-        return data
+    return data
 
 def create_new_order(order_data):
-    user_id = order_data.get('user_id')
-    stock_id = order_data.get('stock_id')
-    type = order_data.get('type')
-    price = order_data.get('price')
-    shares = order_data.get('shares')
+    try:
+        user_id = order_data.get('user_id')
+        stock_id = order_data.get('stock_id')
+        type = order_data.get('type')
+        price = order_data.get('price')
+        shares = order_data.get('shares')
 
-    # Validation Requared
-    if not user_id or not stock_id or not type or not price or not shares:
-        raise ValueError("Invalid Stock data. Please provide all required fields.")
+        # Validation Requared
+        if not user_id or not stock_id or not type or not price or not shares:
+            raise ValueError("Invalid Stock data. Please provide all required fields.")
 
 
-    new_order = Order(user_id=user_id,stock_id=stock_id,type=type,price=price,shares=shares)
+        new_order = Order(user_id=user_id,stock_id=stock_id,type=type,price=price,shares=shares)
 
-    session = Session()
-    session.add(new_order)
-    session.commit()
+        session = Session()
+        session.add(new_order)
+        session.commit()
+        return new_order.id
+    except Exception as e:
+        raise e
 
 def get_user_orders(user_id):
     session = Session()
@@ -50,4 +57,29 @@ def get_user_orders(user_id):
         })
     return data
 
+def delete_order_by_id(order_id):
+    try:
+        session = Session()
+        order = session.query(Order).get(order_id)
 
+        if order:
+            session.delete(order)
+            session.commit()
+            return True
+        else:
+            return False
+    except Exception as e:
+        raise e
+
+def update_order_by_id(order_id, executed):
+    try:
+        session = Session()
+        order = session.query(Order).get(order_id)
+        if order:
+            order.executed = executed
+            session.commit()
+            return True
+        else:
+            return False
+    except Exception as e:
+        raise e
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,18 @@
 from flask import jsonify, request, Blueprint
 
-from .controllers import get_all_orders,create_new_order, get_user_orders
+from app.models import Order, Session
+
+from .controllers import delete_order_by_id, get_all_orders,create_new_order, get_user_orders, update_order_by_id
 
 order_routes = Blueprint("orders", __name__)
 
 @order_routes.route('/',methods=['GET'])
-def index():
+def get_orders():
     try:
         orders = get_all_orders()
-        return jsonify(orders), 200
-    except:
-        return jsonify({"message"}), 400
+        return jsonify({"orders": orders}), 200
+    except Exception as e:
+        return jsonify({"message": "Failed to fetch orders", "error": str(e)}), 400
 
 @order_routes.route('/create',methods=['POST'])
 def create_order():
@@ -21,14 +23,42 @@ def create_order():
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
 
-@order_routes.route('/<int:user_id>',methods=['GET'])
+@order_routes.route('/user/<int:user_id>',methods=['GET'])
 def get_user_orders_by_id(user_id):
     try:
         user_orders = get_user_orders(user_id)
-        if user_orders:
-            return jsonify(user_orders), 200
-        else:
-            return jsonify({"message": "no such user "}), 200
+        return jsonify({"user_orders": user_orders}), 200
+
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
+
+
+@order_routes.route('/<order_id>', methods=['DELETE'])
+def delete_order(order_id):
+    try:
+        delete_result = delete_order_by_id(order_id)
+        if delete_result:
+            return jsonify({"message": "Order deleted successfully"}), 200
+        else:
+            return jsonify({"message": "Order not found"}), 404
+    except Exception as e:
+        return jsonify({"message": "Failed to delete order", "error": str(e)}), 400
+
+@order_routes.route('/<order_id>', methods=['PUT'])
+def update_order(order_id):
+    try:
+        order_data = request.get_json()
+        executed = order_data.get('executed')
+        # Assuming the 'executed' field is included in the request JSON
+
+        update_result = update_order_by_id(order_id, executed)
+        if update_result:
+            return jsonify({"message": "Order updated successfully"}), 200
+        else:
+            return jsonify({"message": "Order not found"}), 404
+    except Exception as e:
+        return jsonify({"message": "Failed to update order", "error": str(e)}), 400
+
+
+
 
